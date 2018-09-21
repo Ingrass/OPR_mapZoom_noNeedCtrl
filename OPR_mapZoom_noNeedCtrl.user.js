b3dfc2ea26c32ab5014c2cb6aff64ff69a2301c6
lokpro <cellurashin@gmail.com>
2018-09-21 18:42:17 +0800

    v1.2 bug fix

--- OPR_mapZoom_noNeedCtrl.user.js
+++ OPR_mapZoom_noNeedCtrl.user.js
@@ -5,22 +5,24 @@
 // @include     https://opr.ingress.com/recon*
 // @downloadURL https://github.com/Ingrass/OPR_mapZoom_noNeedCtrl/raw/master/OPR_mapZoom_noNeedCtrl.user.js
 // @updateURL https://github.com/Ingrass/OPR_mapZoom_noNeedCtrl/raw/master/OPR_mapZoom_noNeedCtrl.user.js
-// @version     1.1
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 
 /*
+v1.2 bug fix
 v1.1 added support for Map in 'Portal Edit'
 */
 
 var interval = setInterval( function(){
 	
 	// wait for information available
-	let map, map2, locationEditsMap;
+	let map, map2, locationEditsMap, subCtrl;
 	try {
-		map = angular.element(document.getElementById('NewSubmissionController')).scope().subCtrl.map;
-		map2 = angular.element(document.getElementById('NewSubmissionController')).scope().subCtrl.map2;
-		locationEditsMap = angular.element(document.getElementById('NewSubmissionController')).scope().subCtrl.locationEditsMap;
+		subCtrl = angular.element(document.getElementById('NewSubmissionController')).scope().subCtrl;
+		map = subCtrl.map;
+		map2 = subCtrl.map2;
+		locationEditsMap = subCtrl.locationEditsMap;
 		
 		if( subCtrl.reviewType==='NEW' ){
 			if( ! map || !map2 ) return;
@@ -34,8 +36,8 @@ var interval = setInterval( function(){
 	clearInterval( interval );
 	
 	if( subCtrl.reviewType==='NEW' ){
-		map.setOptions({ gestureHandling: "greedy"});
-		map2.setOptions({ gestureHandling: "greedy"});
+		map.setOptions({ gestureHandling: "greedy", scaleC11ontrol: true });
+		map2.setOptions({ gestureHandling: "greedy", scale1Control: true});
 	}else{ //subCtrl.reviewType==='EDIT'
 		locationEditsMap.setOptions({ gestureHandling: "greedy"});
 	}
